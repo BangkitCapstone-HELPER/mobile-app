50915d96a358222e50f4dc187a3699a8de6057eb
mhmmdjafarg <mjafarashadiq712@gmail.com>
2022-05-03 23:24:23 +0700

    fix conflict

--- app/src/main/java/com/example/helperstartup/Model/Service/ApiService.kt
+++ app/src/main/java/com/example/helperstartup/Model/Service/ApiService.kt
@@ -6,46 +6,5 @@ import retrofit2.Call
 import retrofit2.http.*
 
 interface ApiService {
-    // post login retrofit
-//    @FormUrlEncoded
-//    @POST("login")
-//    fun login(
-//        @Field("email") email : String,
-//        @Field("password") password: String
-//    ) : Call<LoginResponse>
-//
-//    @FormUrlEncoded
-//    @POST("register")
-//    fun register(
-//        @Field("name") name : String,
-//        @Field("email") email: String,
-//        @Field("password") password: String
-//    ) : Call<SignUpResponse>
-//
-//    @GET("stories")
-//    fun getStories(
-//        @Header("Authorization") token: String,
-//        @Query("location") location : Int?,
-//        @Query("page") page : Int?,
-//        @Query("size") size : Int?
-//    ) : Call<StoriesResponse>
-//
-//    @GET("stories")
-//    suspend fun getStoriesSuspend(
-//        @Header("Authorization") token: String,
-//        @Query("location") location : Int?,
-//        @Query("page") page : Int?,
-//        @Query("size") size : Int?
-//    ) : StoriesResponse
-//
-//
-//    @Multipart
-//    @POST("stories")
-//    fun uploadImage(
-//        @Header("Authorization") token: String,
-//        @Part file: MultipartBody.Part,
-//        @Part("description") description: RequestBody,
-//        @Part("lat") latitude: Double?,
-//        @Part("lon") longitude: Double?
-//    ): Call<AddStoryResponse>
+    
 }
