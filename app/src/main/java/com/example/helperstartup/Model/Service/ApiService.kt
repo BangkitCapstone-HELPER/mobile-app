fde8f81a62a39cfff52b3a56ed078584d87e107e
Jafar Muhammad <56466220+mhmmdjafarg@users.noreply.github.com>
2022-05-04 23:41:43 +0700

    Fix #6 auth user register (#11)
    * add register layout
    
    * complete register

--- app/src/main/java/com/example/helperstartup/Model/Service/ApiService.kt
+++ app/src/main/java/com/example/helperstartup/Model/Service/ApiService.kt
@@ -1,8 +1,10 @@
 package com.example.helperstartup.Model.Service
 
 import com.example.helperstartup.Model.Service.ResponseApi.LoginApiResponse
+import com.example.helperstartup.Model.Service.ResponseApi.RegisterResponse
 import com.example.helperstartup.Model.Service.ResponseApi.UserInfo
 import com.example.helperstartup.Model.Service.ResponseApi.ResponseArticle
+import com.example.helperstartup.Model.Service.request.UserRegister
 import retrofit2.Call
 import retrofit2.http.*
 
@@ -13,6 +15,13 @@ interface ApiService {
     fun login(
         @Body userData : UserInfo
     ) : Call<LoginApiResponse>
+
+    // register
+    @Headers("Content-Type: application/json")
+    @POST("user/")
+    fun register(
+        @Body userData : UserRegister
+    ) : Call<RegisterResponse>
 
     @GET("article/")
     fun getArticle(): Call<ResponseArticle>
