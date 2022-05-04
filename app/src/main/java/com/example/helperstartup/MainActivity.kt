6eb151cb54c50cec2bc5a3b673b6974c3b90ed74
Jafar Muhammad <56466220+mhmmdjafarg@users.noreply.github.com>
2022-05-04 19:21:46 +0700

    Fix #3 auth user login (#10)
    * add usermodel and shared pref
    
    * add loginactivity
    
    * fix conflict
    
    * fix gradle
    
    * fix package name
    
    * complete login

---
+++ app/src/main/java/com/example/helperstartup/MainActivity.kt
@@ -0,0 +1,35 @@
+package com.example.helperstartup
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+//import android.os.Bundle
+//import com.example.helperstartup.databinding.ActivityMainBinding
+//import com.example.helperstartup.model.UserModel
+//import com.example.helperstartup.model.UserPreferenceShared
+//import com.example.helperstartup.view.activity.LoginActivity
+
+class MainActivity : AppCompatActivity() {
+//    private lateinit var binding: Binding
+//    private lateinit var mUserPreference: UserPreferenceShared
+//    private lateinit var userModel: UserModel
+//
+//    override fun onCreate(savedInstanceState: Bundle?) {
+//        super.onCreate(savedInstanceState)
+//        binding = ActivityMainBinding.inflate(layoutInflater)
+//        setContentView(binding.root)
+//        mUserPreference = UserPreferenceShared(this)
+//        showExistingPreference()
+//    }
+//
+//    private fun showExistingPreference() {
+//        userModel = mUserPreference.getUser()
+//        if (!userModel.isLogin) {
+//            startActivity(Intent(this, LoginActivity::class.java))
+//            finish()
+//        }
+//    }
+//
+//    private fun deleteUser() {
+//        mUserPreference.setUser(UserModel("", "", "", false))
+//    }
+}
--- app/src/main/java/com/example/helperstartup/Model/Service/ApiService.kt
+++ app/src/main/java/com/example/helperstartup/Model/Service/ApiService.kt
@@ -1,10 +1,18 @@
 package com.example.helperstartup.Model.Service
 
+import com.example.helperstartup.Model.Service.ResponseApi.LoginApiResponse
+import com.example.helperstartup.Model.Service.ResponseApi.UserInfo
 import com.example.helperstartup.Model.Service.ResponseApi.ResponseArticle
 import retrofit2.Call
 import retrofit2.http.*
 
 interface ApiService {
+    // post login retrofit
+    @Headers("Content-Type: application/json")
+    @POST("user/login")
+    fun login(
+        @Body userData : UserInfo
+    ) : Call<LoginApiResponse>
 
     @GET("article/")
     fun getArticle(): Call<ResponseArticle>
