54b62c45809b009c7416fd098a502e485ce4e53d
Muhammad Fahmi Alamsyah <13519077@std.stei.itb.ac.id>
2022-05-06 06:02:58 +0700

    feat : add image slider

--- app/src/main/java/com/example/helperstartup/View/Catering/home/DetailMenu.kt
+++ app/src/main/java/com/example/helperstartup/View/Catering/home/DetailMenu.kt
@@ -2,11 +2,22 @@ package com.example.helperstartup.View.Catering.home
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.denzcoskun.imageslider.ImageSlider
+import com.denzcoskun.imageslider.models.SlideModel
 import com.example.helperstartup.R
 
 class DetailMenu : AppCompatActivity() {
+    private lateinit var imageSlider : ImageSlider
+    private var slideModel = ArrayList<SlideModel>()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_detail_menu)
+
+        imageSlider = findViewById(R.id.imageSlider)
+        slideModel.add(SlideModel("https://picsum.photos/200/300"))
+        slideModel.add(SlideModel("https://picsum.photos/200/300"))
+        slideModel.add(SlideModel("https://picsum.photos/200/300"))
+        slideModel.add(SlideModel("https://picsum.photos/200/300"))
+        imageSlider.setImageList(slideModel, true)
     }
 }
