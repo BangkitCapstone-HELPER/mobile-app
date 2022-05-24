ec0051be474993b178d2fcd05677a53fe3919326
mhmmdjafarg <mjafarashadiq712@gmail.com>
2022-05-24 11:11:43 +0700

    try rounded

--- app/src/main/java/com/example/helperstartup/Model/helper/DataHelper.kt
+++ app/src/main/java/com/example/helperstartup/Model/helper/DataHelper.kt
@@ -1,30 +1,24 @@
 package com.example.helperstartup.Model.helper
 
 import android.icu.text.NumberFormat
-import android.os.Build
-import android.util.Log
-import androidx.annotation.RequiresApi
 import java.text.SimpleDateFormat
 import java.time.LocalDateTime
 import java.time.format.DateTimeFormatter
 import java.util.*
 
 
-@RequiresApi(Build.VERSION_CODES.N)
 fun getCurrentDate(): String {
     val dateFormat = SimpleDateFormat("yyyy/MM/dd HH:mm:ss", Locale.getDefault())
     val date = Date()
     return dateFormat.format(date)
 }
 
-@RequiresApi(Build.VERSION_CODES.O)
 fun formatDate (date : String): String {
-    val localDateTime = LocalDateTime.parse("2022-05-20T09:55:00")
-    val formatter = DateTimeFormatter.ofPattern("dd-M-yyyy", Locale.getDefault())
+    val localDateTime = LocalDateTime.parse(date)
+    val formatter = DateTimeFormatter.ofPattern("dd MMMM yyyy", Locale.getDefault())
     return formatter.format(localDateTime).toString()
 }
 
-@RequiresApi(Build.VERSION_CODES.N)
 fun formatRupiah(number: Int): String? {
     val localeID = Locale("in", "ID")
     val formatRupiah: NumberFormat = NumberFormat.getCurrencyInstance(localeID)
