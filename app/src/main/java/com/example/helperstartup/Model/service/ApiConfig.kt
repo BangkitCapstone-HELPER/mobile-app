eb46b843602800799e7ca85c02c2904c4bba24a6
https://gitlab.com/13519077 <13519077@std.stei.itb.ac.id>
2022-06-09 19:49:19 +0700

    fixing camera scanner

--- app/src/main/java/com/example/helperstartup/Model/service/ApiConfig.kt
+++ app/src/main/java/com/example/helperstartup/Model/service/ApiConfig.kt
@@ -4,6 +4,7 @@ import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
+import java.util.concurrent.TimeUnit
 
 class ApiConfig {
     companion object {
@@ -13,6 +14,9 @@ class ApiConfig {
                 HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.BODY)
             val client = OkHttpClient.Builder()
                 .addInterceptor(loggingInterceptor)
+                .connectTimeout(5, TimeUnit.MINUTES)
+                .readTimeout(5, TimeUnit.MINUTES)
+                .writeTimeout(5, TimeUnit.MINUTES)
                 .build()
             val retrofit = Retrofit.Builder()
                 .baseUrl(baseUrl)
